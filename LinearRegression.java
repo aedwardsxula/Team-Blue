54a2c7a8300128f91d1bf3b4c27f7662a899ddf1
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-05 15:42:59 -0500

    Adds get correlation method

--- LinearRegression.java
+++ LinearRegression.java
@@ -41,6 +41,22 @@ public class LinearRegression {
         return intercept;
     }
 
-    
+    public double getCorrelation() {
+    int n = x.length;
+    double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        sumX += x[i];
+        sumY += y[i];
+        sumXY += x[i] * y[i];
+        sumX2 += x[i] * x[i];
+        sumY2 += y[i] * y[i];
+    }
+
+    double numerator = n * sumXY - sumX * sumY;
+    double denominator = Math.sqrt((n * sumX2 - sumX * sumX) * (n * sumY2 - sumY * sumY));
+
+    return numerator / denominator;
+}
 
 }
