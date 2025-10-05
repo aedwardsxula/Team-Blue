871809cdd147bb636f9a0ca8fe344968c77c33fd
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-05 10:24:50 -0500

    Adds percentile method

--- StatsHelper.java
+++ StatsHelper.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StatsHelper {
@@ -22,6 +24,17 @@ public class StatsHelper {
                 double max = -Double.MAX_VALUE;
                 for (Number n : list) if (n.doubleValue() > max) max = n.doubleValue();
                 return max;
-            }      
+         
+            }
+    double percentile(List<? extends Number> list, double p) {
+                List<Double> sorted = new ArrayList<>();
+                for (Number n : list) sorted.add(n.doubleValue());
+                Collections.sort(sorted);
+                double idx = p * (sorted.size() - 1);
+                int lower = (int) Math.floor(idx);
+                int upper = (int) Math.ceil(idx);
+                if (upper == lower) return sorted.get(lower);
+                return sorted.get(lower) + (sorted.get(upper) - sorted.get(lower)) * (idx - lower);
 
+            }
 }
