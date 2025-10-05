dbd24f00b6cccf1b3616c80380df68d74a9ad8cb
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-05 10:18:25 -0500

    Adds minimum method

--- StatsHelper.java
+++ StatsHelper.java
@@ -13,5 +13,11 @@ public class StatsHelper {
                 for (Number n : list) sumSq += Math.pow(n.doubleValue() - mean, 2);
                 return Math.sqrt(sumSq / list.size());
             }
+    double min(List<? extends Number> list) {
+                double min = Double.MAX_VALUE;
+                for (Number n : list) if (n.doubleValue() < min) min = n.doubleValue();
+                return min;
+            }
+            
 
 }
