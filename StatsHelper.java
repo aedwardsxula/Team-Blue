7273347dfc3d32e231b0952d80b1e42c569e6d0c
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-05 10:14:34 -0500

    Adds mean method

--- StatsHelper.java
+++ StatsHelper.java
@@ -1,3 +1,12 @@
+import java.util.List;
+
 public class StatsHelper {
+    double mean(List<? extends Number> list) {
+                double sum = 0;
+                for (Number n : list) sum += n.doubleValue();
+                return sum / list.size();
+            }
+
     
+
 }
