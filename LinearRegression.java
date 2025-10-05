79890091ec7df9f37dadbde0570c2c04aeda5c6c
DeAnna Nichols <nicholsdeanna12@gmail.com>
2025-10-05 15:36:25 -0500

    Adds caluculate coefficients method

--- LinearRegression.java
+++ LinearRegression.java
@@ -12,6 +12,23 @@ public class LinearRegression {
         this.x = x;
         this.y = y;
         calculateCoefficients();
+
+    }
+
+    private void calculateCoefficients() {
+        int n = x.length;
+        double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            sumX += x[i];
+            sumY += y[i];
+            sumXY += x[i] * y[i];
+            sumX2 += x[i] * x[i];
+        }
+
+        // Formula for slope (m) and intercept (b)
+        slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        intercept = (sumY - slope * sumX) / n;
     }
 
 }
